eeb3f5f58556e4c56311b75f0f320bca17de87f5
937msk <144563039+937msk@users.noreply.github.com>
2025-09-10 20:37:04 +0530

    Create  If-Else-and-Nested -If-Else statements to determine-whether-a-personi-is-eligible-for-voting.java

---
+++ If-Else-and-Nested -If-Else statements to determine-whether-a-personi-is-eligible-for-voting.java
@@ -0,0 +1,34 @@
+package assigm;
+
+public class Ifelse {
+
+	    public static void main(String[] args) {
+	       
+	        int[] ages = {16, 18, 25};
+	        boolean[] hasValidID = {true, false, true};
+
+	        for (int i = 0; i < ages.length; i++) {
+	            int age = ages[i];
+	            boolean id = hasValidID[i];
+
+	            System.out.println("Testing the Case " + (i+1) + ": Age = " + age + ", Has Valid ID = " + id);
+
+	            if (age >= 18) {
+	            	
+	                
+	                if (id) {
+	                    System.out.println("You are eligible to vote.");
+	                    
+	                } else {
+	                    System.out.println("You are eligible by age, but missing valid identification.");
+	                    
+	                }
+	            } else {
+	                System.out.println("You are not eligible to vote.");
+	                
+	            }
+	            System.out.println("-------------------------");
+	       }
+	   
+	    }
+	}
